8a8eb11ec4d9ef01405838adb3f6850d9d66f575
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-28 16:06:01 +0100

    feat : partie n°3

---
+++ src/Form/TaskType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['class' => 'form'],
+                'required' => true
+            ])
+            ->add('content', TextType::class, [
+                'attr' => ['class' => 'form'],
+                'required' => true
+            ])
+            ->add('createdAt', DateType::class, [
+                'attr' => ['class' => 'form'],
+                'widget' => 'single_text',
+            ])
+            ->add('expiredAt', DateType::class, [
+                'attr' => ['class' => 'form'],
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
