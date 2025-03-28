6d583b237c4e12a5954f4e89bfa64f25f1c853aa
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-28 15:56:08 +0100

    fix taskservice

--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -3,16 +3,24 @@
 namespace App\Service;
 
 use App\Entity\Task;
+use App\Form\TaskType;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TaskService
 {
-    public function __construct(private readonly TaskRepository $taskRepository)
+    public function __construct(private readonly TaskRepository $taskRepository, private readonly EntityManagerInterface $em)
     {
     }
 
     public function addTask(Task $task): void
     {
+
+        $this->em->persist($task);
+        $this->em->flush();
+
+
+
 
     }
 
